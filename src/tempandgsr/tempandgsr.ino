643ffe27e5693c7ee067aecdde5fb9cd89155646
Sriram567 <sriramarumulla567@gmail.com>
2021-11-08 20:47:44 +0530

    Added Hash Function

--- src/tempandgsr/tempandgsr.ino
+++ src/tempandgsr/tempandgsr.ino
@@ -20,12 +20,36 @@ IPAddress subnet(255, 255, 255, 0);
 
 int sensorValue=0;
 int gsr_average=0;
-char * hash(String inp){
-  char *input;
-  inp.toCharArray(input,inp.length());
-  /**
-   * TO BE DONE BY SRI-RAM
-   */
+char * hash(String inp)
+{
+  char *payload;
+  inp.toCharArray(payload,inp.length());
+  byte shaResult[32];
+  mbedtls_md_context_t ctx;
+  mbedtls_md_type_t md_type = MBEDTLS_MD_SHA256;
+
+  const size_t payloadLength = strlen(payload);
+
+  mbedtls_md_init(&ctx);
+  mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(md_type), 0);
+  mbedtls_md_starts(&ctx);
+  mbedtls_md_update(&ctx, (const unsigned char *) payload, payloadLength);
+  mbedtls_md_finish(&ctx, shaResult);
+  mbedtls_md_free(&ctx);
+
+  Serial.print("Hash: ");
+  String ret="";
+  for(int i= 0; i< sizeof(shaResult); i++)
+  {
+  char str[3];
+  sprintf(str, "%02x", (int)shaResult[i]);
+  ret+=str;
+  }
+  Serial.println(ret);
+  return ret;
+  char * outt;
+  ret.toCharArray(outt,ret.length());
+  return outt;
 }
 
 
